7302c4acbca80bd2903acaad5f90c3616ed396f6
Jordan Irlam <irlamco@gmail.com>
2022-03-18 15:30:10 +0000

    First commit

---
+++ variablesOperators.js
@@ -0,0 +1,53 @@
+/* Activity 1:
+
+let firstName = "Jordan";
+let age = "22";
+let favColour = "Orange";
+
+console.log(`My name is ${firstName}, my age is ${age} and my favourite colour is ${favColour}`)
+
+firstName = "Joe"
+age = "25"
+favColour = "Red"
+
+console.log(`My name is ${firstName}, my age is ${age} and my favourite colour is ${favColour}`)
+
+*/
+
+/* Activity 2:
+let breakfast = "Cheese on Toast"
+let lunch = "Chicken Wrap"
+let dinner = "Stir Fry"
+
+console.log(`Day 1 - Breakfast: ${breakfast}, Lunch: ${lunch}, Dinner: ${dinner}`)
+
+breakfast = "Cereal"
+lunch = "Pasta"
+dinner = "Sushi"
+
+console.log(`Day 2 - Breakfast: ${breakfast}, Lunch: ${lunch}, Dinner: ${dinner}`)
+*/
+
+/* Activity 3:
+const birthDate =  new Date('04/09/1999');
+const todaysDate = new Date()
+
+console.log(todaysDate)
+console.log(birthDate)
+
+var timeDifference = todaysDate.getTime() - birthDate.getTime()
+
+var dayDifference = timeDifference / (1000 * 60 * 60 * 24)
+
+console.log(Math.round(dayDifference))
+*/
+
+let space1 = "x"
+let space2 = "o"
+let space3 = ""
+let space4 = "x"
+let space5 = "x"
+let space6 = ""
+let space7 = "o"
+let space8 = ""
+let space9 = ""
