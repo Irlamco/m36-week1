a3cf7bded40cd4147b5cd59a6ce7e7b06d341e00
Jordan Irlam <irlamco@gmail.com>
2022-03-18 15:28:24 +0000

    First commit

---
+++ loops.js
@@ -0,0 +1,78 @@
+/*
+let favSongs = [
+    "NF - The Search", 
+    "Colours - FLOW", 
+    "The Rumbling - SiM"
+];
+    console.log(favSongs)
+favSongs.push("Unravel - TK", "Punky Funky Love - GRANRODEO");
+    console.log(favSongs)
+favSongs.pop()
+    console.log(favSongs)
+*/
+
+/*
+let favDrinks = ["Water","Coke","Dr Pepper"]
+for(let i = 0; i < favDrinks.length; i++){
+    console.log(favDrinks[i])
+}
+*/
+
+/*
+let multiplesFour = [];
+for (let i = 0; i <= 40; i++){
+    if (i % 4 != 0){
+        multiplesFour.push(i);
+    }
+}
+
+console.log(multiplesFour)
+*/
+/*
+let cards = ["Diamond","Spade","Heart","Club"]
+let currentCard = "Club";
+
+while(currentCard != "Spade"){
+    console.log(currentCard);
+    currentCard = cards[Math.floor(Math.random()*4)];
+}
+
+
+console.log(currentCard);
+*/
+
+/* Activity 1
+let films = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+]
+
+films.push("Six","Seven");
+
+for (i = 0; i < films.length; i++){
+    console.log(films[i])
+}
+*/
+
+/* Activity 2
+let nums = []
+
+for (i = 0; i < 6; i++){
+    gen = Math.floor((Math.random() * 50) + 1);
+    nums.push(gen)
+}
+
+console.log(nums)
+*/
+
+/* Activity 3
+let numbs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for (i = numbs.length - 1; i >= 0; i--){
+    numbs.push(i)
+    console.log(numbs[i])
+}
+*/
