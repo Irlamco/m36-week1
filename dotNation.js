dabb5efaa77c5313f53d0d5eaf279d574c0fb630
Jordan Irlam <irlamco@gmail.com>
2022-03-18 17:18:49 +0000

    First commit

---
+++ dotNation.js
@@ -0,0 +1,52 @@
+//Dice Roll
+//console.log(Math.round(Math.random() * 6))
+
+/* Activity 1:
+const table = ["   |   |   ","-----------"];
+for (i=0;i<=3;i++)
+{console.log(table[0]);}
+
+console.log(table[1])
+
+for (i=0;i<=3;i++)
+{console.log(table[0]);}   
+
+console.log(table[1])
+
+for (i=0;i<=3;i++)
+{console.log(table[0]);}
+*/
+
+/* Extra Activity 1:
+const aboutMe = {Name:"Jordan Irlam", Age:"22", Starsign:"Aries"}
+console.log(aboutMe)
+*/
+
+/* Extra Activity 2:
+let string = "My name is Jordan lastname,"
+    let newstring = string.replace(/LaSTnAMe/i, "Irlam")
+let age = 22.11
+    let newage = age.toFixed(0)
+console.log(newstring,"I am ", newage,"years old.")
+*/
+
+// var = function-scoped variables
+// let, const creates block-scoped variables. let can be reasigned, const cannot be reasigned
+
+/* Will work
+let count = 1;
+count = 2;
+
+Will not work
+const count = 1;
+count = 2;
+*/
+
+/* % returns the remainder
+example:
+let x = 7;
+let y = 2;
+let z = x % y;
+console.log(z)
+*/
+
